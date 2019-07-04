d2d4f014070c09c661f246a468df74956a6885e0
Matheus Mandotti <mathew.mfranco@gmail.com>
2019-07-04 13:07:01 -0300

    Add conditional exercises

--- JavaScript/Section11/script.js
+++ JavaScript/Section11/script.js
@@ -1,7 +1,31 @@
-// && AND
-// || OR
-// ! NOT
+function checkAge(){
+  var age = Number(prompt("How old are you?"));
 
-var x = 10;
-var y = "a";
-y ==="b" || x >= 10
+  if ( age < 0 ){
+    console.log("Error, you can't have a negative age");
+  } else if ( age === 21 ){
+    console.log("Happy 21st birthday");
+  } else if ( age%2 != 0 ){
+    console.log("Your age is odd");
+  } else if(age % Math.sqrt(age) === 0) {
+    console.log("Your age is a perfect square!");
+  }
+};
+// checkAge();
+
+function guessTheNum(){
+  var num = Number(prompt("Enter some value: "));
+  var secretNumber = 3;
+  if ( num === secretNumber ){
+    alert("You got it right");
+  } else if ( num > secretNumber ){
+    alert("Too hight, try again");
+    guessTheNum();
+  } 
+  else if ( num < secretNumber ) {
+    alert("Too low, try again");
+    guessTheNum();
+  }   
+};
+
+guessTheNum();
