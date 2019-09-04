9c1d158e9d5471dbdac01bfb0ff3c8752cff1f21
Matheus <mathew.mfranco@gmail.com>
2019-09-03 22:15:13 -0300

    Add ex1 selectors

---
+++ JavaScript/Section15-DOM/ex1.js
@@ -0,0 +1,8 @@
+var tag1 = document.getElementById("first");
+var tag2 = document.getElementsByClassName("special")[0];
+var tag3 = document.getElementsByTagName("p")[0];
+var tag4 = document.querySelector("#first");
+var tag5 = document.querySelector(".special");
+var tag6 = document.querySelector("p");
+var tag7 = document.querySelectorAll(".special")[0];
+var tag8 = document.querySelector("h1 + p");
