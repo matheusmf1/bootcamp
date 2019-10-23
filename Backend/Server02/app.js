490d2258156b3ed051e5cadd23e98ca5aca5e213
Matheus Mandotti <mathew.mfranco@gmail.com>
2019-10-23 13:48:24 -0300

    Add Server test

---
+++ Backend/Server02/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var received;
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res, next ) =>{
+  res.send("Main Page");
+});
+
+app.get('/:operacao', (req, res, next ) =>{
+  res.send("Operations Page");
+});
+
+app.post('/:operacao/dados',( req, res, next ) => {
+  received = req.body;
+  res.json(req.body);
+
+});
+
+app.get('/:operacao/dados',( req, res, next ) => {
+  res.send(received);
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
