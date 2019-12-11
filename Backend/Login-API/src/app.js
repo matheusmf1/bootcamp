ddbbc5fb66851b2e7569c19e88a1de17b3ee0f4e
Matheus Mandotti <mathew.mfranco@gmail.com>
2019-12-11 08:22:12 -0300

    Implement middleware to authenticate request

--- Backend/Login-API/src/app.js
+++ Backend/Login-API/src/app.js
@@ -7,6 +7,7 @@ app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( { extended: false } ) );
 
 require( './controllers/authController' )(app);
+require( './controllers/projectController' )(app);
 //repassa para o controler o app
 
 app.listen( 4000 );
---
+++ Backend/Login-API/src/controllers/projectController.js
@@ -0,0 +1,14 @@
+// Necessario para o usuario fazer as requisições somente quando logado
+
+const express = require('express');
+const authMiddleware = require('../middlewares/auth');
+
+const router = express.Router();
+
+router.use( authMiddleware );
+
+router.get( '/', ( req, res ) => {
+  res.send( { ok: true, user: req.userId } );
+});
+
+module.exports = app => app.use( '/projects', router );
